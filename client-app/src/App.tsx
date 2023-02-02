a1732eab93f785a7593bd23a37a888423109f903
KienQuan <hotrankienquan144@gmail.com>
2023-02-02 17:07:47 +0700

    first commit

---
+++ client-app/src/App.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios';
+import { List } from 'semantic-ui-react';
+function App() {
+  const [data,setData] = useState([])
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/activities")
+      .then(res => {
+        console.log(res.data)
+        setData(res.data)
+    })
+    
+    
+  }, [])
+  console.log("abc")
+  return (
+    <div className="App">
+      <List>
+    
+      {
+        data && data.length > 0 && data.map((d: any )=> (
+      <List.Item>
+          {d.title}
+          </List.Item>
+        ))
+      }
+  </List>
+      
+    </div>
+  );
+}
+
+export default App;
