ce4afa5ecf365c4ae97eecab0e334eec92e899d1
KienQuan <hotrankienquan144@gmail.com>
2023-03-08 22:24:40 +0700

    end section 12 identity

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -4,25 +4,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 using Application.Activities;
+using Microsoft.AspNetCore.Authorization;
 namespace API.Controllers
 {
-    // public class ActivitiesController : BaseApiController
-    // {
-  
-
-    //       [HttpGet] //api/activities
-    //       public async Task<ActionResult<List<Activity>>> GetActivities()
-    //       {
-    //     return await Mediator.Send(new List.Query());
-    //     }
-
-    //   [HttpGet("{id}")] //api/activities/fdfkdffdfd
-    //   public async Task<ActionResult<Activity>> GetActivity(Guid id)
-    //   {
-    //     return await Mediator.Send(new Details.Query{Id = id});
-
-    //   }
-    // }
      public class ActivitiesController : BaseApiController
     {
         [HttpGet]
